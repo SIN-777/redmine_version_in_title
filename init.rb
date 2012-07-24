728d27f51050b78aaf3740b6bbe9576d0f7a9011
SIN-777 <gamblemaster@gmail.com>
2012-07-24 15:09:10 +0900

    initial commit

---
+++ init.rb
@@ -0,0 +1,13 @@
+require 'redmine'
+
+Redmine::Plugin.register :redmine_version_in_title do
+  name 'Redmine Version In Title plugin'
+  author 'Shingo Sekiguchi'
+  description 'Redmine plugin to show current version name to header title.'
+  version '0.0.1'
+  url 'https://github.com/SIN-777/redmine_version_in_title'
+  author_url 'https://github.com/SIN-777/redmine_version_in_title'
+end
+
+require File.dirname(__FILE__) + '/lib/version_in_title_patch.rb'
+ApplicationHelper.send(:include, VersionInTitlePatch)
---
+++ lib/version_in_title_patch.rb
@@ -0,0 +1,25 @@
+module VersionInTitlePatch
+  def self.included(klass)
+    klass.send(:include, InstanceMethods)
+    klass.class_eval do
+      alias_method_chain :page_header_title, :version
+    end
+  end
+
+  module InstanceMethods
+    def page_header_title_with_version
+      title = page_header_title_without_version
+      if @project && !@project.new_record?
+        version = @project.versions.find(
+          :first,
+          :conditions => ['status = "open" and effective_date >= ?', Time.now.to_date],
+          :order => :effective_date
+        )
+        title << %Q| <span style="font-size:75%">= #{version.name} =</span>| if version
+      end
+      title
+    end
+  end
+
+
+end
