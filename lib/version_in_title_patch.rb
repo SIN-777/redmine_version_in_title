e5c8f0a62166eef35c33d387b3a1a6493c8a3e4c
SIN-777 <gamblemaster@gmail.com>
2012-07-24 21:24:02 +0900

    change marker char

--- lib/version_in_title_patch.rb
+++ lib/version_in_title_patch.rb
@@ -15,7 +15,7 @@ module VersionInTitlePatch
           :conditions => ['status = "open" and effective_date >= ?', Time.now.to_date],
           :order => :effective_date
         )
-        title << %Q| <span style="font-size:75%">= #{version.name} =</span>| if version
+        title << %Q| <span style="font-size:75%"> [#{version.name}]</span>| if version
       end
       title
     end
