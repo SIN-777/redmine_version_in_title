a328aa18a1efb3b6393a259d78b118a4650578db
SIN-777 <gamblemaster@gmail.com>
2012-07-26 14:27:28 +0900

    direct style specification should not be there

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-redmine_version_in_title
-========================
+redmine\_version\_in\_title
+===================================
+
+This plugin indicate current version name (which will close in closest day) within the header title.
+
+Version name is enclosed by `<span class="version_in_title"></span>` 
+
+
--- lib/version_in_title_patch.rb
+++ lib/version_in_title_patch.rb
@@ -15,7 +15,7 @@ module VersionInTitlePatch
           :conditions => ['status = "open" and effective_date >= ?', Time.now.to_date],
           :order => :effective_date
         )
-        title << %Q| <span style="font-size:75%"> [#{version.name}]</span>| if version
+        title << %Q| <span class="version_in_title"> [#{version.name}]</span>| if version
       end
       title
     end
